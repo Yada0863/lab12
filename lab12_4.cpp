ce139b31d7630dbdd40f738126ba801e9a0508b1
Yada <yada_pholam@cmu.ac.th>
2023-12-26 15:34:39 +0700

    finished

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename currentType>
+void mySwap(currentType &x,currentType &y);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,13 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+
+template <typename currentType>
+void mySwap(currentType &x,currentType &y){
+	currentType temp =x;
+	x=y;
+	y=temp;
 }
 
